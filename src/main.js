854c56e8caa7e8cec2554319997bea77e9e22138
cmcwil0 <cmcwil0@gmail.com>
2025-08-18 13:10:41 -0500

    Todo list priority function
    added priority button
    added priority label to each task

--- src/main.js
+++ src/main.js
@@ -2,7 +2,27 @@ function setupList() {
   const taskInput = document.querySelector('.task-input');
   const addTaskBtn = document.querySelector('.add-task-btn');
   const taskList = document.querySelector('.task-list');
-  const todoContainer = document.querySelector('.todo-container')
+  const todoContainer = document.querySelector('.todo-container');
+  const priorityBtn = document.querySelector('.priority-btn');
+
+  let priority = 1; //initializes at low (one !)
+
+  function updatePriority() {
+    const icon = priorityBtn.querySelector('i');
+    if(priority === 1) {
+      icon.style.color = "lightgreen";
+    } else if (priority === 2) {
+      icon.style.color = "gold";
+    } else if(priority === 3) {
+      icon.style.color = "lightcoral";
+    }
+  }
+
+  priorityBtn.addEventListener('click', () => {
+    priority++;
+    if(priority > 3) priority = 1;
+    updatePriority();
+  });
   
   function addTask(event) {
     event.preventDefault(); //stops default form event from happening
@@ -12,14 +32,17 @@ function setupList() {
     }
 
     const newTask = document.createElement('li'); //creates new element
+    let priorityColor = 'lightgreen';
+    if(priority === 2) { priorityColor = 'gold';} else if(priority === 3) { priorityColor = 'lightcoral'; }
     newTask.innerHTML = `
     <input type="checkbox" class="checkbox">
+    <div class="priority-label"><i class="fa-solid fa-exclamation" style="color: ${priorityColor};"></i></div>
     <span class="task-text">${taskText}</span>
     <div class="task-buttons">
       <button class="edit-btn"><i class="fa-solid fa-pen"></i></button>
       <button class="delete-btn"><i class="fa-solid fa-trash"></i></button>
     </div>
-    `;
+    `.trim();
 
     taskList.appendChild(newTask);
     taskInput.value = ''
@@ -33,18 +56,17 @@ function setupList() {
     const editBtn = newTask.querySelector('.edit-btn');
     const taskTextElement = newTask.querySelector('.task-text');
 
-   
-
     editBtn.addEventListener('click', () => {
       //if input is already there submit input value
       const editInput = taskTextElement.querySelector('.edit-btn-input');
       if(editInput) {
         taskTextElement.innerHTML = `<span>${editInput.value}</span>`;
-        return
+        return;
       }
 
       const initialText = taskTextElement.textContent;
 
+      //replaces span with input
       taskTextElement.innerHTML = `
         <input type="text" class="edit-btn-input" value="${initialText}">
       `;
@@ -62,14 +84,15 @@ function setupList() {
     });
 
     todoContainer.style.width = taskList.children.length > 0 ? '100%' : '50%';
+
   }
 
-  addTaskBtn.addEventListener('click', addTask);
+  addTaskBtn.addEventListener('click', addTask); //add task event listener
   taskInput.addEventListener('keypress', (e) => {
     if(e.key === 'Enter') {
       addTask(e);
     }
-  })
+  });
 
 }
 
