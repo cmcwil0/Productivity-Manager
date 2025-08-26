13a5e5f377aa0445244651d30c23d9e30c0a00c3
cmcwil0 <cmcwil0@gmail.com>
2025-08-26 12:57:47 -0500

    Added todo list functionality
    added add task button functionality
    
    added priority button functionality
    
    added edit button functionality
    
    added responsive style and layout

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,18 +1,110 @@
 import '../css/Todo.css'
+import { isValidElement, useState } from 'react'
 
 const Todo = () => {
+  const [tasks, setTasks] = useState([])
+  const [inputValue, setInputValue] = useState('')
+  const [priorityLevel, setPriorityLevel] = useState(0)
+  const [editingId, setEditingId] = useState(null) //tracks id of task being edited
+  const [editValue, setEditValue] = useState('')
+
+  const priorityLabels = ['!', '!!', '!!!']
+  const priorityClasses = ['low-priority', 'medium-priority', 'high-priority']
+
+  const handleInputChange = (element) => {
+    setInputValue(element.target.value) 
+  }
+
+  const changePriority = (element) => {
+    element.preventDefault() //prevents <form> from submitting 
+    setPriorityLevel((priorityLevel + 1) % 3) //cycles from 0-2 restarts after 2
+  }
+
+  const handleEditInputChange = element => {
+    setEditValue(element.target.value)
+  }
+
+  const editTask = (id) => {
+    if(editingId === id) {
+      setTasks(tasks.map(task => (
+        task.id === id ? { ...task, text: editValue } : task
+      )))
+      setEditingId(null)
+    } else {
+      const taskToEdit = tasks.find(task => task.id === id)
+      setEditValue(taskToEdit.text)
+      setEditingId(id)
+    }
+  }
+
+  const addTask = (element) => {
+    element.preventDefault()
+    if(inputValue.trim() !== '') {
+      const newTask = {
+        id: Date.now(),
+        text: inputValue,
+        priorityLevel: priorityLevel,
+        completed: false
+      }
+      setTasks([...tasks, newTask])
+      setInputValue('')
+      setPriorityLevel(0)
+    }
+  }
+
   return (
     <div className="todo">
       <h1>Todo List</h1>
+
       <main className='todo-container'>
-        <form action="submit" className='add-task-container'>
-          <input type="text" placeholder='Task...'/>
+
+        <form onSubmit={addTask} className='add-task-container'>
+          <input 
+          type="text" 
+          placeholder='Task...'
+          value={inputValue}
+          onChange={handleInputChange}
+          />
           <div className='button-container'>
-            <button className='priority-button'>!</button>
+            <button className='priority-button' onClick={changePriority}>
+              {priorityLabels[priorityLevel]}
+            </button>
             <button className='add-task-button'>+</button>
           </div>
         </form>
-        <ul className='todo-list'></ul>
+
+
+        <ul className='todo-list'>
+          
+          {tasks.map(task => (
+
+            <li key={task.id} className={priorityClasses[task.priorityLevel]}>
+
+              <span className='priority-indicator'>{priorityLabels[task.priorityLevel]}</span>
+              {editingId === task.id ? ( //if currently editing display input
+                <input 
+                  type="text"
+                  className='edit-input'
+                  value={editValue}
+                  onChange={handleEditInputChange}
+                  autoFocus
+                />
+              ) : ( //otherwise display text
+                <span className='task-text'>{task.text}</span>
+              )
+
+              }
+              <div className='task-buttons-container'>
+                <button className='edit-button'
+                onClick={() => editTask(task.id)}>{editingId === task.id ? '✓' : '✎'}</button>
+                <button className='delete-button'>X</button>
+              </div>
+
+            </li>
+          ))
+
+          }
+        </ul>
       </main>
     </div>
   )
